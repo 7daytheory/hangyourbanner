ca460c8e2cda38e3a1f54604cc07e6a7ee7a40af
Matthew Lowe <matthewjlowe11@gmail.com>
2024-08-18 21:27:39 -0400

    Create general design of banner, rectangle with triangle pointing down at bottom

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div id="banner" className="bg-white h-7 w-7 relative text-center font-semibold text-[16px] text-white leading-[27px] hover:bg-white after:content-[''] after:absolute after:bottom-[-18px] after:left-0 after:h-0 after:w-0 after:border-[15px] after:border-deeppink after:border-b-transparent hover:after:border-purple hover:after:border-b-transparent">
-        Test
-    </div>
+    <div>
+  <div id="banner" className="bg-white h-[350px] w-[200px] m-auto text-black border-solid border-slate-700 border-2 relative text-center font-semibold text-[16px] text-white leading-[27px] hover:bg-white">
+    Test
+    <div class="absolute bottom-[-100px] left-0 w-0 h-0 border-l-[100px] border-r-[100px] border-t-[100px] border-solid border-slate-700 border-2 border-t-gray border-l-transparent border-r-transparent"></div>
+  </div>
+</div>
   )
 }
 
