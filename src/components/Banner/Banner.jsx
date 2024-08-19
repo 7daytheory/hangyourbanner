bf858b1ea270f2c844d4883bfdad372def54a578
Matthew Lowe <matthewjlowe11@gmail.com>
2024-08-19 12:38:05 -0400

    added all nfl teams logos as svgs

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>
-  <div id="banner" className="bg-white h-[350px] w-[200px] m-auto text-black border-solid border-slate-700 border-2 relative text-center font-semibold text-[16px] text-white leading-[27px] hover:bg-white">
-    Test
-    <div class="absolute bottom-[-100px] left-0 w-0 h-0 border-l-[100px] border-r-[100px] border-t-[100px] border-solid border-slate-700 border-2 border-t-gray border-l-transparent border-r-transparent"></div>
+  <div>
+    <div id="banner" className="bg-white h-[350px] w-[200px] m-auto text-black border-solid border-slate-700 border-2 relative text-center font-semibold text-[16px] text-white leading-[27px] hover:bg-white">
+      Test
+      <div class="absolute bottom-[-100px] left-0 w-0 h-0 border-l-[100px] border-r-[100px] border-t-[100px] border-solid border-slate-700 border-2 border-t-gray border-l-transparent border-r-transparent"></div>
+    </div>
   </div>
-</div>
   )
 }
 
