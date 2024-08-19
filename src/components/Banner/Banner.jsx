f0f4983742d567a3534ef2b86c4602f39fec7e77
Matthew Lowe <matthewjlowe11@gmail.com>
2024-08-18 21:03:08 -0400

    Setup tailwindcss and added some styles to banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div id="banner" className="bg-white h-7 w-7 relative text-center font-semibold text-[16px] text-white leading-[27px] hover:bg-white after:content-[''] after:absolute after:bottom-[-18px] after:left-0 after:h-0 after:w-0 after:border-[15px] after:border-deeppink after:border-b-transparent hover:after:border-purple hover:after:border-b-transparent">
+        Test
+    </div>
   )
 }
 
