7b7eb27bb501ffb07d745e6eff1e78f4da669550
Matthew Lowe <matthewjlowe11@gmail.com>
2024-08-22 17:09:09 -0400

    Updated styling for mobile

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -75,7 +75,7 @@ const Banner = () => {
   const [height, setHeight] = useState(400);
   const [bannerText, setBannerText] = useState("Enter Banner Text");
   const [textSize, setTextSize] = useState(24);
-  const [selectedTeam, setSelectedTeam] = useState(`../../src/assets/nfl/${teams[0].city}.svg`); 
+  const [selectedTeam, setSelectedTeam] = useState(ArizonaCardinals); 
   const [teamName, setTeamName] = useState(teams[0].name); 
 
   function setBannerWidth(event) {
@@ -95,11 +95,13 @@ const Banner = () => {
     });
   }
 
-  function handleTeamChange(team) {
-    const selectedTeam = teams.find(t => t.city === team);
-    setSelectedTeam(`../../src/assets/nfl/${selectedTeam.city}.${selectedTeam.format}`);
-    setTeamName({teamName});
-  }
+  function handleTeamChange(e) {
+      const team = e.target.value;
+      const selectedTeam = teams.find(t => t.city === team);
+      console.log(selectedTeam);
+      setSelectedTeam(`${selectedTeam.logo}`);
+      setTeamName(`${selectedTeam.city}`);
+    }
 
   // Download the image
   function downloadURI(uri, name) {
@@ -112,97 +114,101 @@ const Banner = () => {
   }
 
   return (
-        <div className="flex justify-between items-center mx-auto w-[70%] mt-10 relative">
-          <div id="banner-wrap" className='p-2 border-2 border-solid border-slate-700 bg-gray-200'>
-            <div id="banner" style={{ width: `${width}px`, height: `${height}px` }} className="p-4 bg-white text-black border-dashed border-slate-700 border-2 relative text-center font-semibold flex flex-col justify-between items-center">
-              <img src={selectedTeam} alt="Image here" className="h-[100px] w-auto m-auto mt-2" />
-              <p className='mt-2 font-bold text-slate-600 p-2' style={{fontSize: `${textSize}px`}}>{bannerText}</p>
-              <h2 className="font-bold text-[3em] align-bottom text-slate-700 w-[90%] leading-10 mt-auto mb-2">{teamName}</h2>
-            </div>
-          </div>
+    <div className="flex flex-col lg:flex-row justify-between items-center mx-auto w-[70%] mt-10 relative">
+      <div id="banner-wrap" className="p-2 border-2 border-solid border-slate-700 bg-gray-200">
+        <div
+          id="banner"
+          style={{ width: `${width}px`, height: `${height}px` }}
+          className="p-4 bg-white text-black border-dashed border-slate-700 border-2 relative text-center font-semibold flex flex-col justify-between items-center"
+        >
+          <img src={selectedTeam} alt="Team Logo" className="h-[100px] w-auto m-auto mt-2" />
+          <p className="mt-2 font-bold text-slate-600 p-2" style={{ fontSize: `${textSize}px` }}>
+            {bannerText}
+          </p>
+          <h2 className="font-bold text-[3em] text-slate-700 w-[90%] leading-10 mt-auto mb-2">
+            {teamName}
+          </h2>
+        </div>
+      </div>
 
-          <div id="sliders" className="ml-6 w-[40%]">
-          <div 
-            id="button" 
-            onClick={printImage} 
-            className="mt-4 px-4 py-2 bg-slate-600 hover:bg-slate-700 text-white text-center mb-8 w-[50%] rounded m-auto cursor-pointer"
+      <div id="sliders" className="w-full lg:w-[60%] mt-8 lg:mt-0 lg:ml-6">
+        <div 
+          id="button" 
+          onClick={printImage} 
+          className="mt-4 px-4 py-2 bg-slate-600 hover:bg-slate-700 text-white text-center mb-8 w-full lg:w-[50%] rounded m-auto cursor-pointer"
+        >
+          Download Banner
+        </div>
+        <div className="mb-4">
+          <h2>Banner Text</h2>
+          <input
+            id="banner-text-input"
+            type="input"
+            value={bannerText}
+            onChange={(e) => setBannerText(e.target.value)}
+            className="w-full bg-gray-200 rounded-lg cursor-pointer px-2 py-1 mb-8"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="team-select" className="mr-4">Select Logo</label>
+          <select 
+            id="team-select" 
+            value={teamName} 
+            onChange={handleTeamChange}
+            className="w-full bg-gray-200 rounded-md cursor-pointer bg-white border-2 border-slate-400"
           >
-            Download Banner
-          </div>
-          <div className="relative">
-              <h2>Banner Text</h2>
-              <input
-                id="labels-range-input banner-height"
-                type="input"
-                value={bannerText}
-                onChange={(e) => setBannerText(e.target.value)}
-                className="w-full bg-gray-200 rounded-lg appearance-none cursor-pointer px-2 py-1 bg-gray-200 mb-8"
-                />
-            </div>
-            <div className="mb-4">
-            <label htmlFor="team-select" className="mr-4">Select Logo</label>
-            <select 
-              id="team-select" 
-              value={selectedTeam} 
-              onChange={(e) => handleTeamChange(e.target.value)}
-              className="w-full bg-gray-200 rounded-md cursor-pointer bg-white border-2 border-slate-400"
-            >
-              {teams.map((team, index) => (
-                <option key={index} value={team.city}>
-                  {team.city}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="relative mb-10">
-              <h2>Text Size</h2>
-              <input 
-                id="labels-range-input banner-width" 
-                type="range" 
-                value={textSize} 
-                min="8" 
-                max="42"
-                onChange={(e) => setTextSize(e.target.value)}
-                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer bg-gray-700"
-              />
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">8px</span>
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">48px</span>
-            </div>
-            <div className="relative mb-10">
-              <h2>Banner Width</h2>
-              <input 
-                id="labels-range-input banner-width" 
-                type="range" 
-                value={width} 
-                min="250" 
-                max="400"
-                onChange={setBannerWidth}
-                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer bg-gray-700"
-              />
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">Smallest</span>
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">Smaller</span>
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-2/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">Larger</span>
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">Largest</span>
-            </div>
-            <div className="relative">
-              <h2>Banner Height</h2>
-              <input 
-                id="labels-range-input banner-height" 
-                type="range" 
-                value={height} 
-                min="315" 
-                max="600"
-                onChange={setBannerHeight}
-                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-                />
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">Smallest</span>
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">Smaller</span>
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-2/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">Larger</span>
-              <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">Largest</span>
-            </div>
-          </div>
-        </div> 
-  )
-}
+            {teams.map((team, index) => (
+              <option key={index} value={team.city}>
+                {team.city}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-10">
+          <h2>Text Size</h2>
+          <input 
+            id="text-size-range-input" 
+            type="range" 
+            value={textSize} 
+            min="8" 
+            max="42"
+            onChange={(e) => setTextSize(e.target.value)}
+            className="w-full h-2 bg-gray-200 rounded-lg cursor-pointer"
+          />
+          <span className="text-sm text-gray-500 absolute start-0 -bottom-6">8px</span>
+          <span className="text-sm text-gray-500 absolute end-0 -bottom-6">42px</span>
+        </div>
+        <div className="mb-10">
+          <h2>Banner Width</h2>
+          <input 
+            id="banner-width-range-input" 
+            type="range" 
+            value={width} 
+            min="250" 
+            max="400"
+            onChange={setBannerWidth}
+            className="w-full h-2 bg-gray-200 rounded-lg cursor-pointer"
+          />
+          <span className="text-sm text-gray-500 absolute start-0 -bottom-6">Smallest</span>
+          <span className="text-sm text-gray-500 absolute end-0 -bottom-6">Largest</span>
+        </div>
+        <div className="mb-10">
+          <h2>Banner Height</h2>
+          <input 
+            id="banner-height-range-input" 
+            type="range" 
+            value={height} 
+            min="315" 
+            max="600"
+            onChange={setBannerHeight}
+            className="w-full h-2 bg-gray-200 rounded-lg cursor-pointer"
+          />
+          <span className="text-sm text-gray-500 absolute start-0 -bottom-6">Smallest</span>
+          <span className="text-sm text-gray-500 absolute end-0 -bottom-6">Largest</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
