26ab4673c0317c5854df0a0ff60d2f510fa231d0
Matthew Lowe <matthewjlowe11@gmail.com>
2024-08-25 01:25:36 -0400

    Added email popup using emailJS

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ContactModal from '../ContactModal/ContactModal';
+
 
 const Navigation = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
   return (
+    <>
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -9,16 +21,23 @@ const Navigation = () => {
         </a>
         <div className="flex items-center space-x-4 md:order-2">
           <button type="button" className="px-4 py-2 text-sm font-medium text-gray-300 dark:text-gray-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-300" disabled>Donate (soon)</button>
-          <a href="/contact" className="px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">Contact</a>
+          <button
+              type="button"
+              onClick={openModal}
+              className="px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
+              Contact
+            </button>
           <button type="button" data-dropdown-toggle="language-dropdown-menu" className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700">
-            <svg className="w-5 h-5 rounded-full me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" strokeWidth="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use xlinkHref="#a" y="420"/><use xlinkHref="#a" y="840"/><use xlinkHref="#a" y="1260"/></g><use xlinkHref="#a" y="1680"/></g><use xlinkHref="#b" x="247" y="210"/></g><use xlinkHref="#c" x="494"/></g><use xlinkHref="#d" x="988"/><use xlinkHref="#c" x="1976"/><use xlinkHref="#e" x="2470"/></g></svg>
-            English (More Soon)
+            Beta (1.0.0)
           </button>
           <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700" id="language-dropdown-menu">
           </div>
         </div>
       </div>
     </nav>
+    <ContactModal isOpen={isModalOpen} onClose={closeModal} />
+    </>
   );
 };
 
