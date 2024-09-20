c75356ddb2a7eae8e6fb5fa0709effab57154c41
Matthew Lowe <matthewjlowe11@gmail.com>
2024-09-20 14:43:14 -0400

    Added Fade to the footer as well

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../assets/logo.svg'
+import { Fade } from 'react-awesome-reveal';
 
 const Footer = () => {
     const date = new Date(); 
@@ -9,22 +10,31 @@ const Footer = () => {
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div className="sm:flex sm:items-center sm:justify-between">
                 <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <img src={logo} className="h-8" alt="Hang Your Banner Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Hang Your Banner</span>
+                    <img src={logo} className="absolute h-[75px] w-auto" alt="Hang Your Banner Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white pl-[60px]">Hang Your Banner</span>
                 </a>
                 <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <Fade duration={750} cascade>
                     <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                        <a href="#" className="hover:underline me-4 md:me-6">Examples</a>
                     </li>
                     <li>
                         <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
                     </li>
                     <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                        <a href="#" className="hover:underline me-4 md:me-6">Best Banners</a>
                     </li>
+                    <button
+                        type="button"
+                        href="best"
+                        className="md:me-6 me-4 text-sm font-medium hover:text-gray-300 disabled"
+                        >
+                        Animate Banner
+                    </button>
                     <li>
                         <a href="#" className="hover:underline">Contact</a>
                     </li>
+                    </Fade>
                 </ul>
             </div>
             <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-6" />
