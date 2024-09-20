ba1b19651f196bb2405183a38d9a0b11e275cbc9
Matthew Lowe <matthewjlowe11@gmail.com>
2024-09-20 13:08:12 -0400

    Update button on banner so logo can be larger

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import ContactModal from '../ContactModal/ContactModal';
 import logo from "../../assets/logo.svg";
 
-
 const Navigation = () => {
   const [isModalOpen, setModalOpen] = useState(false);
 
@@ -13,13 +12,23 @@ const Navigation = () => {
   const closeModal = () => {
     setModalOpen(false);
   };
+
   return (
     <>
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 max-h-[70px]">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <img src={logo} className="h-[125px] w-auto mt-[-20px] ml-[150px]" alt="Hang Your Banner Logo" />
-        <div className="flex items-center space-x-4 md:order-2 mt-[-65px]">
-        <button
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 max-h-[70px]">
+        <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+          {/* Logo Section */}
+          <div className="flex items-center">
+            <img 
+              src={logo} 
+              className="h-[150px] w-auto absolute top-0 left-[100px]" 
+              alt="Hang Your Banner Logo" 
+            />
+          </div>
+
+          {/* Navigation Buttons */}
+          <div className="flex items-center space-x-4">
+            <button
               type="button"
               href="examples"
               className="px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
@@ -33,7 +42,7 @@ const Navigation = () => {
             >
               Donate(Soon)
             </button>
-          <button
+            <button
               type="button"
               onClick={openModal}
               className="px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
@@ -47,12 +56,10 @@ const Navigation = () => {
             >
               Your Account(Soon)
             </button>
-          <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700" id="language-dropdown-menu">
           </div>
         </div>
-      </div>
-    </nav>
-    <ContactModal isOpen={isModalOpen} onClose={closeModal} />
+      </nav>
+      <ContactModal isOpen={isModalOpen} onClose={closeModal} />
     </>
   );
 };
