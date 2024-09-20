f530e2cb7d44ffba824088f49fea3e7dd62e5708
Matthew Lowe <matthewjlowe11@gmail.com>
2024-09-20 12:59:16 -0400

    Created a logo for the site - display on page

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ContactModal from '../ContactModal/ContactModal';
+import logo from "../../assets/logo.svg";
 
 
 const Navigation = () => {
@@ -14,18 +15,37 @@ const Navigation = () => {
   };
   return (
     <>
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 max-h-[70px]">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Hang Your Banner</span>
-        </a>
-        <div className="flex items-center space-x-4 md:order-2">
+          <img src={logo} className="h-[125px] w-auto mt-[-20px] ml-[150px]" alt="Hang Your Banner Logo" />
+        <div className="flex items-center space-x-4 md:order-2 mt-[-65px]">
+        <button
+              type="button"
+              href="examples"
+              className="px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
+              Examples
+            </button>
+            <button
+              type="button"
+              href="#"
+              className="px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
+              Donate(Soon)
+            </button>
           <button
               type="button"
               onClick={openModal}
               className="px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
             >
               Contact
+            </button>
+            <button
+              type="button"
+              href="#"
+              className="px-4 py-2 text-sm font-medium text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
+              Your Account(Soon)
             </button>
           <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700" id="language-dropdown-menu">
           </div>
