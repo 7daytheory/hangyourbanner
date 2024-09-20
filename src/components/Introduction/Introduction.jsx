e7f5b95b38d1819144f3942eec5ede814d861803
Matthew Lowe <matthewjlowe11@gmail.com>
2024-09-20 18:38:12 -0400

    Added a color-picker tool to the color picker

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Introduction = () => {
   return (
     <>
-      <div className="flex flex-col w-full mt-4 justify-center items-center text-center mt-[60px] lg:mt-4">
+      <div className="flex flex-col w-full mt-4 justify-center items-center text-center mt-[60px] lg:mt-4 p-2">
         <h1 className="text-red-800 text-[2em] smtext-[1.5em] font-bold tracking-wide">Hang Your Banner</h1>
         <h2 className="text-lg lg:text-2xl mt-4">Create banners for the teams that need one!</h2>
         <p className="text-base lg:text-lg mt-4 max-w-4xl">
