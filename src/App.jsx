e5c02cc8ba4fb27131f5472458231db29f0fd326
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-31 15:31:17 -0400

    Fix the call for switch causing an error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 // Components Imports
 import HomeLayout from './components/layouts/HomeLayout';
 import Banner from "./components/Banner/Banner";
-import { Route, BrowserRouter as Router, Routes, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Introduction from './components/Introduction/Introduction';
 
 function App() {
